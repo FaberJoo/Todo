4b56730b30798a32e0ee6f496d12fd07f8a064bb
FaberJoo <sjdk0805@gmail.com>
2023-02-08 14:25:14 +0900

    [변경] Entity JPA 어노테이션 적용

--- src/main/java/com/codestates/todo/todo/Todo.java
+++ src/main/java/com/codestates/todo/todo/Todo.java
@@ -1,4 +1,27 @@
 package com.codestates.todo.todo;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Todo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 30, nullable = false)
+  private String title;
+
+  @Column(nullable = false, unique = false)
+  private int todo_order;
+
+  @Column
+  private boolean completed;
 }
